71732cebef6ccf9694d98ae0ee44f7a58dc9284d
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-10-25 17:43:43 +0000

    feat: adding post interactions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,11 @@ class PostsController < ApplicationController
   def index
     @blog_posts = Post.all
   end
+
+  def show
+    @blog_post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    # caso ocorra o erro do active record nao encontrar redireciona para a pagina principal
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+  # get "posts/:id", to: "posts#show"
+  # get "posts/:id", to: "posts#show", as: :posts
+
+  resources :posts, only: [ :index, :show ]
 end
